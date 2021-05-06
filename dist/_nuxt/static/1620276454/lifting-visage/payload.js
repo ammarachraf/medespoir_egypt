e47213e0d7ba0e575f2b31367386e1d474f92b41
OunalliMohamed <mohamed.ounelli@touchlink.fr>
2021-05-06 05:49:37 +0100

    Commit v1.3 dist + pages

---
+++ dist/_nuxt/static/1620276454/lifting-visage/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/lifting-visage", {data:[{}],fetch:{},mutations:void 0});
---
+++ dist/_nuxt/static/1620276454/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Flifting-visage","\u002Fabdominoplastie","\u002Fblepharoplastie"]})
