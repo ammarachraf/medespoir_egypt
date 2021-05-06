12a7f5bc86db581a8b8e4479312dffd7843593d7
OunalliMohamed <mohamed.ounelli@touchlink.fr>
2021-05-06 05:34:54 +0100

    Commit v1.3 pages

---
+++ pages/blepharoplastie.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="__pages">
+        <div class="top__title abdo">
+            <div class="containerXD">
+                <div class="row">
+                    <div class="col-lg-5 col-md-5 col-12 display-flex">
+                        <div class="width-60 text-center">
+                            <h1>Blépharoplastie Egypte</h1>
+                            <div class="btn_devis mt-4"><NuxtLink to="/devis">Devis express</NuxtLink></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="container">
+            <div class="row">
+                <div class="col-md-5 col-12"><img src="@/assets/img/img1-blepharoplastie.jpg" alt="Medespoir maroc"></div>
+                <div class="col-md-7 col-12 flex-item">
+                    <p class="color-blue text-center">La blépharoplastie, également appelée chirurgie des paupières, est une intervention de chirurgie esthétique destinée à rajeunir le contour des yeux du visage. Elle implique souvent l'élimination de l'excès de graisse et de peau des paupières supérieures et inférieures. La chirurgie peut aider à corriger les signes de vieillissement et le regard fatigué des paupières supérieures tombantes et des poches gonflées sous les yeux.<br>Elle est souvent associée à d'autres interventions esthétiques, telles que le lifting du visage ou le lipofilling visage pour améliorer chirurgicalement l'apparence du visage.</p>
+                    <h2 class="yellow">Prix blépharoplastie Egypte</h2>
+                    <p>Pour une chirurgie paupières prix pas cher, notre clinique esthétique Egypte vous propose une blépharoplastie prix tout compris. Toutes les dépenses liées à votre séjour esthétique en Egypte seront couvertes (clinique, hôtel, transport, soins médicaux…).<br>Veuillez demander un devis en ligne pour en savoir plus sur la blépharoplastie tarif réduit.</p>
+                </div>
+            </div>
+        </section>
+        <section class="devis_express mt-5 pt-5 pb-5">
+            <div class="container">
+                <h2 class="yellow mb-3 text-center">Devis Express en ligne</h2>
+                <iframe frameborder="0" src="https://forms.zohopublic.com/highlight/form/MedespoircomExpress/formperma/kTOAYYPldfLDE4I67OaiiMq9xcRQzDPZLGGgnsSlQ8k" style="height:420px;width:100%;border:none"></iframe>
+            </div>
+        </section>
+        <section class="container mb-5">
+            <div class="row">
+                <div class="col-md-7 col-12">
+                    <h2 class="text-left yellow mt-5">Comment se préparer à l’opération des paupières Egypte?</h2>
+                    <p class="text-left color-blue">La blépharoplastie est une intervention personnalisée qui nécessite une visite préopératoire approfondie. Lors de la première visite, le chirurgien doit évaluer la structure osseuse et musculaire du visage, la qualité de la peau et des muscles périoculaires et la présence d'éventuels défauts esthétiques et fonctionnels et illustrer les objectifs avec les résultats esthétiques pouvant être obtenus.<br>Il doit également évaluer l’état de santé de son patient pour écarter la présence d'altérations (hypertension artérielle, problèmes de coagulation ou de cicatrisation) qui pourraient affecter le résultat final de la chirurgie. A l'issue de la visite, le chirurgien, en accord avec le patient, choisit l'intervention chirurgicale la plus adaptée.</p>
+                    <h3 class="color-blue">Conseils préopératoires</h3>
+                    <ul class="liste-page color-blue">
+                        <li>Ne pas prendre les médicaments contenant de l’aspirine ou de la vitamine E deux semaines avant l’opération.</li>
+                        <li>Arrêter de fumer au moins une semaine avant la chirurgie.</li>
+                    </ul>
+                </div>
+                <div class="col-md-5 col-12"><img src="@/assets/img/img2-blepharoplastie.jpg" alt="Medespoir egypte"></div>
+            </div>
+        </section>
+        <section class="bg-blue pt-5 pb-5">
+            <div class="container">
+                <h2 class="yellow mb-4">Qui est le bon candidat pour la chirurgie esthétique des paupières?</h2>
+                <p>Les meilleurs candidats à la chirurgie des paupières sont les hommes et les femmes dont la peau a commencé à s'affaisser mais a conservé une bonne élasticité.<br>Ils doivent être également en bonne santé, psychologiquement stables et bien informés sur l’intervention chirurgicale et post-chirurgicale, exigences communes à toutes les interventions de chirurgie esthétique.<br>Généralement, les personnes de plus de 35 ans ont besoin d'une chirurgie paupières pour rajeunir leurs yeux tandis que les patients plus jeunes en ont souvent besoin pour corriger les facteurs génétiques.</p>
+                <h2 class="yellow mb-4">Quand envisager une chirurgie des paupières Egypte ? </h2>
+                <p>La peau autour des yeux est extrêmement fine et la paupière supérieure, au fil des ans, peut être tombante et peut présenter une ptose profonde ou une chute vers le bas, tandis que la paupière inférieure peut être entourée, en raison du vieillissement, d'une série de rides qu'elles seront circulaire autour de l'œil. Ces deux imperfections donnent au regard et au visage dans son ensemble un aspect ridé, fatigué et vieux. C'est là que le patient envisage une chirurgie esthétique des yeux.<br>Une ptose sévère des paupières peut réduire le champ de vision. Dans ce cas, il est conseillé subir une blépharoplastie supérieure non seulement pour des raisons esthétiques mais aussi pour des raisons fonctionnelles.</p>
+                <h2 class="yellow mb-4">Déroulement de l’intervention</h2>
+                <h3 class="color-white">Blépharoplastie supérieure</h3>
+                <p>Avec une opération des paupières supérieures, il est possible d'éliminer l'excès de peau et les sacs de graisse qui rendent le regard constamment fatigué et gonflé. Tout d'abord, il est essentiel que l'élimination de l'excès de tissu ne soit pas particulièrement extrême car, dans ce cas, le résultat final pourrait être artificiel et désagréable. En ce qui concerne la durée de l'opération, 15 à 20 minutes par œil suffisent.<br>Pour la correction des paupières supérieures, une incision est faite exactement dans le pli de la paupière pour éviter que des cicatrices ne soient visibles à l'avenir. Le chirurgien enlève alors une partie du tissu cutané, puis il ferme l’incision avec une suture et enfin il applique des pansements.</p>
+                <h3 class="color-white">Blépharoplastie inférieure</h3>
+                <p>La blépharoplastie inférieure est intervention esthétique, pratiquée sur les paupières inférieures, qui consiste à enlever ou repositionner les poches de graisse situées dans la région de la paupière inférieure. Pour se faire, le chirurgien pratique une incision dans la bordure sous-ciliaire à travers laquelle les trois poches graisseuses localisées dans cette zone et l'excès de peau et de muscles ont enlevés et remodelés.<br>Cette technique est plus complexe que la blépharoplastie supérieure et elle peut également être réalisée sous anesthésie locale ou sous anesthésie générale.</p>
+                <div class="btn_devis mt-4 text-center"><NuxtLink to="/devis">Devis express</NuxtLink></div>
+            </div>
+        </section>
+        <section class="container">
+            <div class="row">
+                <div class="col-md-5 col-12"><img src="@/assets/img/img3-blepharoplastie.jpg" alt="Medespoir maroc"></div>
+                <div class="col-md-7 col-12">
+                    <h2 class="yellow mt-5">Résultats de la blépharoplastie en Egypte</h2>
+                    <p class="color-blue">Le résultat de la blépharoplastie Egypte est généralement durable mais n’est pas permanent car il est toujours conditionné par le mode de vie et les effets inévitables de la gravité et du vieillissement.<br>Le résultat, appréciable déjà après les trois premières semaines, est définitif 6 mois après l’opération.</p>
+                    <h2 class="yellow mt-5">Pourquoi la vision est-elle floue après une blépharoplastie ?</h2>
+                    <p class="color-blue">La vision floue postopératoire est en grande partie due aux gouttes ophtalmiques lubrifiantes que le chirurgien recommande. Cette pommade ophtalmique est utilisée en postopératoire pour éviter un dessèchement excessif des yeux, et elle peut parfois provoquer une vision floue accidentelle. Le chirurgien recommande généralement d'utiliser cette pommade pendant environ 3 à 5 jours après la chirurgie.</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+<script>
+    export default({
+    data() {
+      return {
+        title: 'Blépharoplastie Egypte'
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Medespoir Egypte Blépharoplastie Egypte' }
+        ],
+        htmlAttrs: {
+            lang: 'fr'
+        },
+      }
+    }
+})
+</script>
