c7cd54f1e4a99a2653504a138eb53649587b42f2
OunalliMohamed <mohamed.ounelli@touchlink.fr>
2021-05-11 07:00:53 +0100

    ct nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,5 @@
 export default {
-    // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
-    // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         link: [
             { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
@@ -9,23 +7,14 @@ export default {
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
         ]
     },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
         '@/assets/css/style.scss'
     ],
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
         { src: 'plugins/owl.js', ssr: false }
     ],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
-        // https://go.nuxtjs.dev/typescript
         '@nuxt/typescript-build',
         '@nuxtjs/device',
         '@nuxtjs/pwa',
@@ -33,30 +22,20 @@ export default {
     device: {
         refreshOnResize: true
     },
-    // Modules: https://go.nuxtjs.dev/config-modules
     modules: [
         [
             '@nuxtjs/device',
             { refreshOnResize: true }
         ],
-        // https://go.nuxtjs.dev/bootstrap
         'bootstrap-vue/nuxt',
-        // https://go.nuxtjs.dev/axios
         '@nuxtjs/axios',
-        // https://go.nuxtjs.dev/pwa
         '@nuxtjs/pwa',
     ],
-
-    // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {},
-
-    // PWA module configuration: https://go.nuxtjs.dev/pwa
     pwa: {
         manifest: {
             lang: 'fr'
         }
     },
-
-    // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {}
 }
